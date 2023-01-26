5f69554cd11dff8645ced1c960b43f33250de266
Vanshita Roopchandani <vanshita@192.168.1.5>
2023-01-27 03:16:44 +0530

    Banner added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbarp } from './components/Navbarp';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Banner} from './components/Banner'
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
   return (
     <div className="App">
       <Navbarp/>
+      <Banner/>
     </div>
   );
 }
